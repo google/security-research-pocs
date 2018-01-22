9210d0c8057d05cf6c308c3f613d6d419aab72cf
Autofuzz <noreply@google.com>
2018-01-22 01:53:48 -0800

    Fuzz target sync. Fuzz target sync.
    PiperOrigin-RevId: 182747453

--- autofuzz/tinyxml_fuzzer.cc
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "tinyxml.h"
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  std::string s(reinterpret_cast<const char*>(data), size);
-  TiXmlDocument doc("fuzz.xml");
-  doc.Parse(s.c_str());
-  return 0;
-}
